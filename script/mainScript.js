46c068ca324fc7e2bdcca41a3b6309e446f771c7
brbrbrbrbr <lumos.colorum@gmail.com>
2018-04-20 13:59:13 +0300

    adding scripts

---
+++ script/mainScript.js
@@ -0,0 +1,51 @@
+new Vue({
+    el: '#carusel',
+    data:{
+        slides: [
+            /*'./images/karusel/1.png',
+            './images/karusel/2.png',
+            './images/karusel/3.png',
+            './images/karusel/4.png',
+            './images/karusel/5.png',*/
+            'https://lorempixel.com/800/400/food/1',
+            'https://lorempixel.com/800/400/food/2',
+            'https://lorempixel.com/800/400/food/3',
+            'https://lorempixel.com/800/400/food/4',
+            'https://lorempixel.com/800/400/food/5',
+        ],
+        current: 0,
+        width: 795,
+        timer: 0,
+    },
+    methods: {
+        nextSlide: function () {
+            this.current++;
+            if (this.current >= this.slides.length)
+                this.current = 0;
+            this.resetPlay();
+        },
+        prevSlide: function () {
+            this.current--;
+            if (this.current < 0)
+                this.current--;
+            this.resetPlay();
+        },
+        selectSlide: function (i) {
+            this.current = i;
+            this.resetPlay();
+        }
+        resetPlay: function () {
+            clearInterval(this.timer);
+            this.play();
+        },
+        play: function () {
+            let app = this;
+            this.timer = setInterval(function () {
+                app.nextSlide();
+            }, 2000);
+        }
+    },
+    created: function () {
+        this.play();
+    }
+});
